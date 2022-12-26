d9fc8ec629532125094862454dccddcd4c84e2a6
Phonevilai <tock.tock.t@gmail.com>
2022-12-26 21:37:23 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Phonevilai/assessment
+
+go 1.19
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- server.go
+++ server.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 	"os"
 )
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Errorf("please consider environment variables: %s\n", err)
+	}
+}
 
 func main() {
 	fmt.Println("Please use server.go for main file")
